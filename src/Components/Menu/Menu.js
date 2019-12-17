0b62747e07e33f9b66705c08442051a88e61341b
Carlos Claro <carlosclaro79@gmail.com>
2019-12-17 17:02:15 -0300

    novo menu

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -4,6 +4,7 @@ import PreLoader from '../PreLoader/PreLoader'
 import Img from 'react-image';
 import M from 'materialize-css';
 
+
 class MenuItens extends Component {
 
 
@@ -13,23 +14,25 @@ class MenuItens extends Component {
     let itens = this.props.menu.map((item) => {
         return (
             <li key={item.link}>
-              <LinkWrapper key={`${item.link}-a`} to="#!" className="dropdown-trigger black-text text-darken-2" data-target={`dropdown-${item.link}`}>{item.titulo} <i className="material-icons right black-text text-darken-2"></i></LinkWrapper>
-              <ul id={`dropdown-${item.link}`} className="dropdown-content">
+              <LinkWrapper key={`${item.link}-a`} to="#" className=" black-text text-darken-2" data-target={`dropdown-${item.link}`}>{item.titulo} <i className="material-icons right black-text text-darken-2"></i></LinkWrapper>
+              <ul id={`dropdown-${item.link}`} className="">
                 <li>
-                  <LinkWrapper key={`int-${item.id}`} to={`imoveis-${item.link}-${this.props.cidadeLink}`} className="dropdown-trigger black-text text-darken-2" data-target={`dropdown-${item.link}`}>{`${item.titulo} Imóveis em ${this.props.cidadeNome}`} <i className="material-icons right black-text text-darken-2"></i></LinkWrapper>
+                  <LinkWrapper key={`int-${item.id}`} to={`imoveis-${item.link}-${this.props.cidadeLink}`} className="black-text text-darken-2">{`${item.titulo} Imóveis em ${this.props.cidadeNome}`} <i className="material-icons right black-text text-darken-2"></i></LinkWrapper>
                 </li>
-
                 {
-                    item.itens.map((sub) => {
-                        return(
-                            <li key={sub.link}>
-                              <LinkWrapper key={sub.link} to={`${item.link}-${sub.link}-${this.props.cidadeLink}`} className="text-darken-2">{sub.titulo}</LinkWrapper>
-                            </li>
-                          )
-                        })
-                      }
-                    </ul>
-                  </li>
+                item.itens.map((sub) => {
+                  console.log(sub);
+                  return(
+                    <li key={sub.link}>
+                      <LinkWrapper key={sub.link} to={`${item.link}-${sub.link}-${this.props.cidadeLink}`} className="black-text text-darken-2" title={`${item.titulo} ${sub.descricao} em ${this.props.cidadeNome}`}>
+                        {sub.descricao}
+                      </LinkWrapper>
+                    </li>
+                  )
+                })
+                }
+              </ul>
+            </li>
                 )
               })
               console.log(itens);
